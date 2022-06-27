129d9a264e119ef8184bb204b738aa19d163f57b
vijaydev133 <vijaydeveloper004@gmail.com>
2022-06-27 08:49:30 +0530

    Adding code.

---
+++ main.js
@@ -0,0 +1,81 @@
+/***************************************************************************** */
+
+// 1. Find output of 10+20+"30"
+
+//Ans: 3030
+
+//Explanation: Because, whenever javascript see a number with "+", it'll
+//try to add that number, so 10+20= 30, and whenever javascrit see a string with
+//"+", it'll try to concatenate, so 30 + "30" = 3030
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 2. Find output of "10"+20+30
+
+//Ans: 102030
+
+//Explanation: So here the javascript encounters the string first so it'll concatenate
+// everything irrespective of the datatypes.
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 3. 10=='10'
+
+//Ans: true
+
+//Explanation: Because, "==" will check only the values
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 4. 20 === "20"
+
+//Ans: false
+
+//Explanation: Because, "===" will check  the values and the datatypes
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 5. JS advantage
+
+//Ans:*) There are some ways to use JavaScript through Node.js servers. 
+//It is possible to develop a whole JavaScript app from front to back using only JavaScript.
+//*)Simplicity. JavaScript is relatively simple to learn and implement.
+//*)Speed. Client-side JavaScript is very fast because it can be run immediately within the client-side browser
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 6. For in and for each difference
+
+//Ans: For in  loop is used to get the index of an array, but forEach is
+// used to get the value and we can manipulate it according to our needs, and it'll 
+//never change the original array
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 7. What is closure?
+
+//Ans: function along with its lexical environment, which means function with its 
+// siblings
+
+/***************************************************************************** */
+/***************************************************************************** */
+
+// 8.Write 5 math inbuilt method.
+
+console.log(Math.random()*5);
+console.log(Math.max(1,9,7,3))
+console.log(Math.min(1,9,7,3))
+console.log(Math.floor(3.9));
+console.log(Math.round(3.9));
+
+/***************************************************************************** */
+
+
+
+
